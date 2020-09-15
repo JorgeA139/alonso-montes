07a838b5fefabb837af7c779ba10014c93d6684d
María Obdulia González Fernández <67651322+obdulia-gonzalez@users.noreply.github.com>
2020-09-15 13:06:27 -0500

    Agrego linea
    Se agrego la linea de hola mundo hithub

--- ejercicio_1/src/ejercicio_1/Ejercicio_1.java
+++ ejercicio_1/src/ejercicio_1/Ejercicio_1.java
@@ -17,6 +17,7 @@ public class Ejercicio_1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("hola mundo");
+        System.out.print("hola mundo de GitHub");
     }
     
 }
